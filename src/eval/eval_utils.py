942ac897ce7f99bd1cb35978a9a7c06ce5288fdf
Brando Miranda <brandojazz@gmail.com>
2024-04-10 22:08:11 -0700

    about to do a big refactor removing train and eval utils

--- src/eval/eval_utils.py
+++ src/eval/eval_utils.py
@@ -1,29 +1,29 @@
-import math
-from typing import Optional, Any, Dict
+# import math
+# from typing import Optional, Any, Dict
 
-# import torch
+# # import torch
 
-# import datasets
-# from datasets import load_dataset, interleave_datasets
+# # import datasets
+# # from datasets import load_dataset, interleave_datasets
 
-# from transformers import PreTrainedTokenizer, AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments, AutoConfig
-from transformers import Trainer 
+# # from transformers import PreTrainedTokenizer, AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments, AutoConfig
+# from transformers import Trainer 
 
-def eval_hf(trainer: Trainer, 
-            path: str, 
-            name: str, 
-            split: str, 
-            eval_dataset = None,  # provide eval data set to overwrite it
-            max_eval_samples: Any = 'Unknown_Eval_Max_Samples',
-            ) -> Dict[str, float]:
-    metrics: Dict[str, float] = trainer.evaluate(eval_dataset)
-    try:
-        perplexity = math.exp(metrics["eval_loss"])
-    except OverflowError:
-        perplexity = float("inf")
-    metrics["perplexity"] = perplexity
-    path = path.replace('/', '_')  # needed only when saving results
-    print(f'Eval metrics {path} {name} {split} {max_eval_samples}: {metrics=}')
-    trainer.log_metrics(f"eval_{path}_{name}_{split}_{max_eval_samples}", metrics)  # display metrics
-    trainer.save_metrics(f"eval_{path}_{name}_{split}_{max_eval_samples}", metrics)
-    return metrics
+# def eval_hf(trainer: Trainer, 
+#             path: str, 
+#             name: str, 
+#             split: str, 
+#             eval_dataset = None,  # provide eval data set to overwrite it
+#             max_eval_samples: Any = 'Unknown_Eval_Max_Samples',
+#             ) -> Dict[str, float]:
+#     metrics: Dict[str, float] = trainer.evaluate(eval_dataset)
+#     try:
+#         perplexity = math.exp(metrics["eval_loss"])
+#     except OverflowError:
+#         perplexity = float("inf")
+#     metrics["perplexity"] = perplexity
+#     path = path.replace('/', '_')  # needed only when saving results
+#     print(f'Eval metrics {path} {name} {split} {max_eval_samples}: {metrics=}')
+#     trainer.log_metrics(f"eval_{path}_{name}_{split}_{max_eval_samples}", metrics)  # display metrics
+#     trainer.save_metrics(f"eval_{path}_{name}_{split}_{max_eval_samples}", metrics)
+#     return metrics
--- src/train/simple_train.py
+++ src/train/simple_train.py
@@ -7,7 +7,9 @@ import evaluate
 from typing import Dict, Tuple, Optional
 from pathlib import Path
 
-from eval.eval_utils import eval_hf
+from utils import eval_hf
+
+from utils import load_model_block_size
 
 
 def compute_metrics(eval_pred: Tuple[np.ndarray, np.ndarray],
@@ -47,9 +49,102 @@ def preprocess_function_proofnet(examples: Dict[str, list], tokenizer: GPT2Token
     model_inputs["labels"] = labels
     return model_inputs
 
+def setup_and_train_big_modol(pretrained_model_name_or_path: str = "mistralai/Mistral-7B-Instruct-v0.2",
+                            path: str = "all_ai4m_datasets_v0",
+                            output_dir_train: str = '~/tmp/all_ai4m_datasets_v0/train',
+                            output_dir_test: str = '~/tmp/all_ai4m_datasets_v0/eval/proofnet/test',
+                            path_to_save_model: Optional[str] = '~/tmp/all_ai4m_datasets_v0/model',  # suggested path: '~/tmp/proofnet/model' then expanduser in py code
+                            max_steps: int = 50,
+                            per_device_train_batch_size: Optional[int] = 2,
+                            per_device_eval_batch_size: Optional[int] = 1,
+                            save_total_limit: Optional[int] = None,
+                            learning_rate: float = 5e-5,
+                            weight_decay: float = 0.01,
+                            max_grad_norm: float = 1.0, 
+                            optim='paged_adamw_32bit',
+                            gradient_accumulation_steps = 8, # see: based on alpaca https://github.com/tatsu-lab/stanford_alpaca, allows to process effective_batch_size = gradient_accumulation_steps * batch_size, num its to accumulate before opt update step
+                            gradient_checkpointing: Optional[bool] = True,  # If True, use gradient checkpointing to save memory at the expense of slower backward pass.
+                            lr_scheduler_type='cosine',
+                            warmup_ratio=0.01,
+                            evaluation_strategy='no',
+                            eval_steps = None,  # TODO
+                            report_to: str = 'wandb',
+                    ) -> None:
+    # Clear CUDA cache to free up memory
+    torch.cuda.empty_cache()
+
+    # Load tokenizer and model
+    model, tokenizer = load_model_block_size(pretrained_model_name_or_path, verbose=True)
+
+    # Load the dataset
+    dataset_val = load_dataset(path, split='validation')
+    dataset_test = load_dataset(path, split='test')
+
+    # Preprocess the dataset
+    if path == "hoskinson-center/proofnet":
+        preprocess_function = preprocess_function_proofnet
+        # note: text field is usually more common!
+        # val_dataset = dataset_val.map(lambda examples: preprocess_function(examples, tokenizer), batched=True, remove_columns=["nl_statement", "formal_statement"])
+        test_dataset = dataset_test.map(lambda examples: preprocess_function(examples, tokenizer), batched=True, remove_columns=["nl_statement", "formal_statement"])
+
+    # Training arguments
+    output_dir_train: Path = Path(output_dir_train).expanduser()
+    output_dir_train.mkdir(parents=True, exist_ok=True)
+    training_args = TrainingArguments(
+        output_dir=output_dir_train,
+        max_steps=max_steps,
+        evaluation_strategy=evaluation_strategy,  # "no"`: No evaluation is done during training. no can be good to avoid memory issues.
+        gradient_accumulation_steps=gradient_accumulation_steps,  # based on alpaca https://github.com/tatsu-lab/stanford_alpaca, allows to process effective_batch_size = gradient_accumulation_steps * batch_size, num its to accumulate before opt update step
+        gradient_checkpointing = gradient_checkpointing,  # TODO depending on hardware set to true?
+        learning_rate=learning_rate,
+        per_device_train_batch_size=per_device_train_batch_size,
+        per_device_eval_batch_size=per_device_eval_batch_size,
+        weight_decay=weight_decay,
+        save_steps=max_steps//3,  # alpaca does 2000, other defaults were 500
+        save_total_limit=save_total_limit,
+        max_grad_norm=max_grad_norm,
+        optim=optim,
+        logging_dir=output_dir_train / 'logs',
+        logging_first_step=True,
+        logging_strategy='steps',
+        eval_steps = eval_steps,
+        remove_unused_columns=False,  # TODO don't get why https://stackoverflow.com/questions/76879872/how-to-use-huggingface-hf-trainer-train-with-custom-collate-function/76929999#76929999 , https://claude.ai/chat/475a4638-cee3-4ce0-af64-c8b8d1dc0d90
+        # lr_scheduler_type=lr_scheduler_type  # TODO: https://discord.com/channels/879548962464493619/1227708244697284724/1227708244697284724
+        # warmup_ratio=warmup_ratio,
+        report_to = report_to,  # options I recommend: 'none', 'wandb'
+        fp16=False,  # never ever set to True
+        bf16=torch.cuda.get_device_capability(torch.cuda.current_device())[0] >= 8,  # if >= 8 ==> brain float 16 available or set to True if you always want fp32
+    )
+
+    # Initialize the Trainer
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=test_dataset,
+        tokenizer=tokenizer,
+        compute_metrics=compute_metrics
+    )
+    # Train the model
+    trainer.train()
+
+    # Evaluate the model
+    if output_dir_test is not None:
+        output_dir_test: Path = Path(output_dir_test).expanduser()
+        output_dir_test.mkdir(parents=True, exist_ok=True)
+        eval_args = TrainingArguments(output_dir=output_dir_test, fp16=False, bf16=torch.cuda.get_device_capability(torch.cuda.current_device())[0] >= 8, report_to=report_to)
+        trainer = Trainer(model=model, args=eval_args, train_dataset=None, eval_dataset=test_dataset)
+        # results: dict[str, float] = trainer.evaluate(test_dataset)
+        results: dict[str, float] = eval_hf(trainer, name='', path=path, split='test', eval_dataset=test_dataset)
+        print(f'{path=} split=test {results=}')
+
+    # Save the trained model
+    if path_to_save_model is not None:
+        model.save_pretrained(path_to_save_model)
+
 def setup_and_train_proofnet(pretrained_model_name_or_path: str = "gpt2", 
                             path: str = "hoskinson-center/proofnet",
-                            output_dir_train: str = '~/tmp/proofnet/train',
+                            output_dir_: str = '~/tmp/proofnet/train',
                             output_dir_val: Optional[str] = None,  # we are training on the val set so no val set
                             output_dir_test: str = '~/tmp/proofnet/test',
                             path_to_save_model: Optional[str] = None,  # suggested path: '~/tmp/proofnet/model' then expanduser in py code
@@ -102,6 +197,7 @@ def setup_and_train_proofnet(pretrained_model_name_or_path: str = "gpt2",
     # Preprocess the dataset
     if path == "hoskinson-center/proofnet":
         preprocess_function = preprocess_function_proofnet
+        # note: text field is usually more common!
         val_dataset = dataset_val.map(lambda examples: preprocess_function(examples, tokenizer), batched=True, remove_columns=["nl_statement", "formal_statement"])
         test_dataset = dataset_test.map(lambda examples: preprocess_function(examples, tokenizer), batched=True, remove_columns=["nl_statement", "formal_statement"])
 
