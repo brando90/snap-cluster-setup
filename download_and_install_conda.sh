cb44309e3fcc9c076ff2e85f9793421c8b124672
Brando Miranda <brando9@stanford.edu>
2024-07-15 15:13:13 -0700

    Update download_and_install_conda.sh

--- download_and_install_conda.sh
+++ download_and_install_conda.sh
@@ -4,7 +4,7 @@ cd $HOME
 
 # -- Install miniconda
 # get conda from the web
-#wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
+#curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 # source conda with the bash command and put the installation at $HOME/miniconda
 bash $HOME/miniconda.sh -b -p $HOME/miniconda
